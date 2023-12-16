d11b00c9cabace335b77dec6f7b3effb3cab6720
armanahad6207 <armanahad7549@gmail.com>
2023-12-16 09:42:45 -0800

    about console() alert() prompt() and confirm()

---
+++ 3alertconsole.js
@@ -0,0 +1,18 @@
+// console.log() => debugging or to display output
+// alert() => for Notification
+// prompt() => taking input from User
+// confirm() => it check whether yes or no
+
+// alert("form submitted successfuly");
+
+// console.log(confirm("are you above 18+ ?"))
+//ok =true value return
+//cancle = false value return
+
+var a=prompt("enter ffirst number");
+var b=prompt("enter second number");
+// prompt return string
+sum=Number(a)+Number(b);
+alert(`sum of two number is:${sum}`);
+
+
