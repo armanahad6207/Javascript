f9aba272f37b7c02ce68f4a0928056716d9e0684
armanahad6207 <armanahad7549@gmail.com>
2023-12-16 09:44:38 -0800

    about string literals

---
+++ 4stringliterals.js
@@ -0,0 +1,31 @@
+// string literals or template literals
+// "An Avengers is an Action movie with rating of 4.5 and from the category Hollywood"
+
+var mname = "Avengers";
+var type = "Action";
+var category = "Hollywood";
+var rating = 4.5;
+
+//es5
+
+var output =
+  " An " +
+  mname +
+  " is an " +
+  " type " +
+  "movie with rating of " +
+  rating +
+  " and from the category of " +
+  category +
+  ".";
+console.log(output);
+
+// es6
+
+// `` => backtick
+
+// ${} => interpolation =>substitute the value
+
+output = `An ${mname} is an ${type} movie with rating of ${rating} and from the category ${category}`;
+
+console.log(output)
