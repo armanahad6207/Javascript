e5f730ab58c9fae3d02027fb3f832b11e6dee803
armanahad6207 <armanahad7549@gmail.com>
2023-12-20 07:13:28 -0800

    else_if and ternary operator

---
+++ 7ifelse.js
@@ -0,0 +1,29 @@
+let a=20;
+if(a%2==0){
+    console.log(`number ${a} is even`);
+}
+else{
+    console.log(`number ${a} is odd`)
+}
+
+let uname="peter";
+if(uname =="jack"){
+    console.log(`hi ${uname} you are  admin`);
+}
+else if(uname=="john"){
+    console.log(`hi ${uname} you are admin`);
+}
+else{
+    console.log(`hi ${uname} is anknown`);
+}
+
+// ternary operator (single line if else)
+
+
+var b=10;
+var c=20;
+var d=40;
+ (b>c)?console.log(`${b} is greater `):console.log(`${c} is greater`)
+
+
+
