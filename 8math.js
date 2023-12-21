3ab714bada2c2b5a7a0470bd3feabfd03490f43b
armanahad6207 <armanahad7549@gmail.com>
2023-12-20 19:47:12 -0800

    Mathe

---
+++ 8math.js
@@ -0,0 +1,42 @@
+console.log(Math.random());
+// it will generate automatic 0.9838071004590376
+
+console.log(Math.random()*1000);
+// 343.4364584842102    
+
+
+
+Math.floor(10.1);
+//10
+
+let a=Math.floor(20.78);
+//20
+console.log(a)
+
+Math.ceil(10.6)
+//11
+Math.ceil(10.1)
+//11
+
+console.log(Math.floor(Math.random()));//0
+console.log(Math.ceil(Math.random()));//1
+Math.round(10.1);
+//10
+Math.round(10.6)
+//11
+console.log(Math.round(12.5));
+//13
+console.log(Math.round(10.4))//10
+
+
+console.log(Math.random() * (45-12));
+console.log(Math.floor(Math.random() * (45 - 12)) + 12); //12
+
+console.log(Math.tan(1))//1.5574077246549023
+console.log(Math.sin(2))//0.9092974268256817
+console.log(Math.pow(3,2))//9
+console.log(Math.log(0))//-imfinity
+console.log(Math.log(1))//0
+console.log(Math.E)//2.718281828459045
+console.log(Math.PI)//3.141592653589793
+console.log(Math.PI.toFixed(2))//3.14
