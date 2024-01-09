6eb9b4c373668a5cda6b9878e1cfa1d6cbeb5af9
armanahad6207 <armanahad7549@gmail.com>
2024-01-08 22:00:29 -0800

    forEach and For Of

--- 12loop.js
+++ 12loop.js
@@ -82,3 +82,90 @@ else{
 // Orange
 //  parent-array => London
 //  parent-array => Pune
+
+// while loop
+let q=0
+while( q<10){
+    console.log(q)
+    q++;
+}
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+
+//  do while
+
+var e=0
+do{
+    console.log("this code will execute once at a time whether the condition is tru or false")
+    
+}while(e!=0)
+// this code will execute once  at a time whether the condition is tru or false
+
+//for of 
+var city = [
+    "Dubai",
+    "Innsburg",
+    "France",
+     ["Red", "Yellow", "Orange"],
+    "London",
+    "Pune",
+  ];
+
+
+  for(mycity of city){
+    console.log(mycity);
+  }
+//   Dubai
+// Innsburg
+// France
+// [ 'Red', 'Yellow', 'Orange' ]
+// London
+// Pune
+
+for(mycity of city){
+    if(Array.isArray(mycity)){
+        for(colony of mycity){
+            console.log(`colony = ${colony}` )
+        }
+    }
+    else{
+        console.log(mycity);
+    }
+}
+// Dubai
+// Innsburg
+// France
+// colony = Red
+// colony = Yellow
+// colony = Orange
+// London
+// Pune
+
+// forEach
+
+var arr=[1,2 ,34,56 ,78,67];
+
+
+ arr.forEach(function (val){
+    console.log(val);
+ }
+);
+ 
+// 1
+// 2
+// 34
+// 56
+// 78
+// 67
+ 
+ 
+ 
+
