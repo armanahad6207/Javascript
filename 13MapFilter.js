bcce5bf9a51875d285d9dfc452df6c3f612c6c51
armanahad6207 <armanahad7549@gmail.com>
2024-01-11 19:37:57 -0800

    update filter()

--- 13MapFilter.js
+++ 13MapFilter.js
@@ -19,9 +19,37 @@ var arr=[12,3,5,6,8,9,23,5];
 
 
 var a = [4, 8, 5, 23];
-var b = a.map((item) => {
+var b= a.map((item) => {
   return `<p>${item}</p>`;
 });
 console.log(b);//[ '<p>4</p>', '<p>8</p>', '<p>5</p>', '<p>23</p>' ]
 
 
+//filter
+//it is used to filter out the value
+//it may or may not return same lenght of output array as input array
+//return only those data which output or condition is true
+
+var arr=[12,34,56,79,23,17]
+
+const check=arr.filter((data)=>{
+    if(data%2==0){
+      return data;
+    }
+   
+})
+
+console.log(check);
+// [ 12, 34, 56 ]
+
+
+const dbl=arr.filter((data)=>{
+     return data*2;
+})
+console.log(`dbl of an array =  ${dbl}`);
+//dbl of an array =  12,34,56,79,23,17
+
+//callback => when you call function inside another function is call callback function
+
+
+//a.map( () => {} )
