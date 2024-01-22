c8391eb68bd6cd4698c4097b5109fd94b3c38b66
armanahad6207 <armanahad7549@gmail.com>
2024-01-22 10:44:37 +0530

    let var

---
+++ let_const.js
@@ -0,0 +1,17 @@
+
+//var is introduce before es6
+// var message="helo! world"
+// var a=(()=>{
+//     var message="hi! world"
+//   console.log(message)
+// });
+
+// a();
+// console.log(message);  =>message is not defined since message is local scope
+
+// we can not aceess local scope from the outside of that scope
+
+// console.log(a) ReferenceError: Cannot access 'a' before initialization
+var b=20;
+ let a=10;
+ console.log(a)//10
