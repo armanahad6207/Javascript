f2e3eb7bcec012e6725f8a19ff3297750001ae73
armanahad6207 <armanahad7549@gmail.com>
2024-01-27 12:03:03 +0530

    callback

--- callbackFunction.js
+++ callbackFunction.js
@@ -1,7 +1,16 @@
 // A callback is a function that is passed as an argument to another function
-  setTimeout(function xyz(){
-     console.log("after 4 second")
-  },4000)
+
+
+// Callbacks are used to handle the results of asynchronous 
+// operations in a non-blocking manner. Asynchronous operations
+//  are operations that take a significant amount of time to complete,
+//   such as network requests, file I/O, and database queries. 
+//   If these operations were executed synchronously,
+//    the program would freeze and wait for the operation to complete before
+//     continuing. This can lead to a poor user experience, as the program would 
+//     appear unresponsive./
+
+
 function x(y){
     console.log("x function")
     y();
@@ -10,3 +19,9 @@ function x(y){
 x( function y(){
     console.log("y function")
 });
+
+setTimeout(function xyz(){
+    console.log("after 4 second")
+ },4000);
+
+//  => function xyz is a also a callback function
