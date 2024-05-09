83cac714f97455f70ebe305b553f8792fa25fd63
armanahad6207 <armanahad7549@gmail.com>
2024-05-09 10:58:22 +0530

    password generarot function

--- passwordGenerator/script.js
+++ passwordGenerator/script.js
@@ -1,9 +1,44 @@
-let textpass = document.querySelectorAll("#password");
-let range = document.querySelectorAll("#rangetarget");
-let rangevalue = document.querySelectorAll("#rangeslider");
+let textpass = document.querySelector("#password");
+let range = document.querySelector("#rangetarget");
+let rangevalue = document.querySelector("#rangeslider");
+let inclowercase = document.querySelector("#lowercase");
+let incuppercase = document.querySelector("#uppercase");
+let incnumber = document.querySelector("#numbers");
+let incsymbol = document.querySelector("#symbols");
+
+//  describe lenght or character or range of password
 rangevalue.textContent = range.value;
 range.addEventListener("input", () => {
   rangevalue.textContent = range.value;
 });
 
-console.log("hello");
+// cominations of passwords
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = "0123456789";
+const symbol = "@#$%&*";
+
+// generate passwordm function
+function generatePassword() {
+  var allchars = "";
+  var genPassword = "";
+  let finalpassword = "";
+  let i = 0;
+  allchars += inclowercase.checked ? lowercase : "";
+  allchars += incuppercase.checked ? uppercase : "";
+  allchars += incnumber.checked ? number : "";
+  allchars += incsymbol.checked ? symbol : "";
+  genPassword = allchars;
+
+  while (i < range.value) {
+    finalpassword =
+      finalpassword +
+      genPassword.charAt(Math.floor(Math.random() * genPassword.length));
+    i++;
+  }
+  textpass.value = finalpassword;
+}
+
+passwordbtn.addEventListener("click", () => {
+  generatePassword();
+});
