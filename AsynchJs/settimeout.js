2ca4eb56e57bc48c4ded47a08461aa646d11653d
armanahad6207 <armanahad7549@gmail.com>
2024-11-30 16:37:31 +0530

    setTimeout asyncFunction

---
+++ AsynchJs/settimeout.js
@@ -0,0 +1,8 @@
+console.log("it will execute instant the progame execute")
+setTimeout(()=>{
+    console.log("it will execute after 2 second")
+},2000)
+
+setTimeout(function(){
+console.log("It will execute after 1 second")
+},1000)
